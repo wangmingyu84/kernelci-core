4a9c5ca80854293674a3d9dbbf40efc4e4165e23
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-12-07 22:32:24 +0000

    kernelci.build: add reldir argument to Step._run_make()
    Add an extra optional argument "reldir" to the Step._run_make()
    method, which is to be joined with self._kdir to form the full
    directory path where to run "make".  The resulting value is passed to
    the "make -C" option.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/build.py
+++ kernelci/build.py
@@ -441,15 +441,16 @@ def _output_to_file(cmd, log_file, rel_dir=None):
     return cmd
 
 
-def _run_make(kdir, arch, target=None, jopt=None, silent=True, cc='gcc',
-              cross_compile=None, use_ccache=None, output=None, log_file=None,
-              opts=None, cross_compile_compat=None):
+def _run_make(subdir=None, arch=None, target=None, jopt=None, silent=True,
+              cc='gcc', cross_compile=None, use_ccache=None, output=None,
+              log_file=None, opts=None, cross_compile_compat=None):
     args = ['make']
 
     if opts:
         args += ['='.join([k, v]) for k, v in opts.items()]
 
-    args += ['-C{}'.format(kdir)]
+    if subdir:
+        args += ['-C{}'.format(subdir)]
 
     if jopt:
         args.append('-j{}'.format(jopt))
@@ -478,7 +479,7 @@ def _run_make(kdir, arch, target=None, jopt=None, silent=True, cc='gcc',
     elif cc != 'gcc':
         args.append('CC={}'.format(cc))
 
-    if output != kdir:
+    if subdir and (output != subdir):
         # due to kselftest Makefile issues, O= cannot be a relative path
         args.append('O={}'.format(os.path.abspath(output)))
 
@@ -593,15 +594,18 @@ class Step:
         cmd = 'grep -cq CONFIG_{}=y {}'.format(config_name, dot_config)
         return shell_cmd(cmd, True)
 
-    def _run_make(self, target, jopt=None, verbose=False, opts=None):
+    def _run_make(self, target, jopt=None, verbose=False, opts=None,
+                  reldir=None):
         env = self._bmeta['environment']
         make_opts = env['make_opts'].copy()
         if opts:
             make_opts.update(opts)
         if jopt is None:
             jopt = int(shell_cmd("nproc")) + 2
+        subdir = os.path.join(self._kdir, reldir) if reldir else self._kdir
+
         return _run_make(
-            self._kdir, env['arch'], target, jopt, not verbose,
+            subdir, env['arch'], target, jopt, not verbose,
             env['compiler'], env['cross_compile'], env['use_ccache'],
             self._output_path, self._log_path, make_opts,
             env['cross_compile_compat'])
