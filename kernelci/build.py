8dc36961aaa21989d52c30ea169b7327d73976bc
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-12-21 23:20:14 +0000

    kernelci.build: add Step.install() base method
    Add a Step.install() method to install the files related to a build
    step.  The default behaviour is to install the build meta-data, with
    bmeta.json and steps.json.  The installation directory is created as
    an _install_ sub-directory in the output path.  It is removed with
    reset=True, typically when initialising the build meta-data
    (RevisionData step).
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- kernelci/build.py
+++ kernelci/build.py
@@ -522,6 +522,8 @@ class Step:
         self._output_path = output_path or os.path.join(kdir, 'build')
         if not os.path.exists(self._output_path):
             os.mkdir(self._output_path)
+        self._install_path = os.path.join(self._output_path, '_install_')
+        self._create_install_dir()
         self._bmeta_path = os.path.join(self._output_path, 'bmeta.json')
         self._steps_path = os.path.join(self._output_path, 'steps.json')
         self._bmeta = self._load_json(self._bmeta_path, dict())
@@ -537,6 +539,11 @@ class Step:
         """Path to the build meta-data JSON file"""
         return self._bmeta_path
 
+    @property
+    def install_path(self):
+        """Path to the installation directory"""
+        return self._install_path
+
     def _load_json(self, json_path, default):
         data = default
         if os.path.exists(json_path):
@@ -546,6 +553,12 @@ class Step:
                 with open(json_path) as json_file:
                     data = json.load(json_file)
         return data
+
+    def _create_install_dir(self):
+        if self._reset:
+            shutil.rmtree(self._install_path, ignore_errors=True)
+        if not os.path.exists(self._install_path):
+            os.makedirs(self._install_path)
 
     def _add_run_step(self, name, status, jopt=None):
         start_time = datetime.fromtimestamp(self._start_time).isoformat()
@@ -671,6 +684,15 @@ class Step:
             cmd = self._output_to_file(cmd, self._log_path)
         return shell_cmd(cmd, True)
 
+    def _install_file(self, path, dest_name=None, verbose=False):
+        if path and os.path.exists(path):
+            if dest_name is None:
+                dest_name = os.path.basename(path)
+            dest_path = os.path.join(self._install_path, dest_name)
+            if verbose:
+                print("Installing {}".format(dest_path))
+            shutil.copy(path, dest_path)
+
     def is_enabled(self):
         """Determine whether the step is enabled with the current kernel."""
         return True
@@ -678,6 +700,18 @@ class Step:
     def run(self):
         """Abstract method to run the build step."""
         raise NotImplementedError("Step.run() needs to be implemented.")
+
+    def install(self, verbose=False):
+        """Base method to install the build artifacts.
+
+        The default behaviour is to install bmeta.json and steps.json in the
+        output install directory.  Sub-classes should call this parent method
+        to have them installed too.
+
+        *verbose* is whether to show what is being installed
+        """
+        for path in [self._log_path, self._bmeta_path, self._steps_path]:
+            self._install_file(path, verbose=verbose)
 
 
 class RevisionData(Step):
@@ -995,7 +1029,8 @@ class MakeSelftests(Step):
         """Make the kernel selftests
 
         Make the kernel selftests or "kselftest" and produce a tarball so they
-        can be installed on a separate test platform.
+        can be installed on a separate test platform.  This step does not add
+        any extrabuild meta-data.
 
         *jopt* is the `make -j` option which will default to `nproc + 2`
         *verbose* is whether the build output should be shown
